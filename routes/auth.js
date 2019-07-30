9ad16831601845777d7cfbb290238f8ea9b668b7
adauren <frankcawperwood818@gmail.com>
2019-07-30 09:49:25 +0600

    Created auth middleware, Contact model, get contact route for specific user

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,18 @@ const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 
 const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 router.post(
