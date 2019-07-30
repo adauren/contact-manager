00257adb97a99a3da8fce62a9d986373c1fd9ce2
adauren <frankcawperwood818@gmail.com>
2019-07-30 09:24:57 +0600

    Database created and connected, created User model and created register route, login route with jwt

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 
 const port = process.env.PORT || 5000;
 
